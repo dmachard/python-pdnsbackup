b4fb52554b3b35126ae6dd3b3adee42e97bdde19
dmachard <5562930+dmachard@users.noreply.github.com>
2023-10-02 10:25:22 +0200

    add sudo in test

--- tests/test_import.py
+++ tests/test_import.py
@@ -3,7 +3,7 @@ import subprocess
 
 class TestImport(unittest.TestCase):
     def test_import(self):
-        cmd = ["python3", "-c", "import pdnsbackup; pdnsbackup.run();"]
+        cmd = ["sudo", "python3", "-c", "import pdnsbackup; pdnsbackup.run();"]
 
         with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as proc:
             output = proc.communicate()[0].decode("utf-8")
